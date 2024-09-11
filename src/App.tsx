dac6e2de959a2e10db9fa86cbb1df3abb35ce5e7
Danilo Mourelle <danilomourelle@outlook.com>
2024-09-11 13:23:15 -0300

    feat(start-page):create the entry start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import logo from "./assets/logo-in-orbit.svg";
+import letsStart from "./assets/lets-start-illustration.svg";
+import { Plus } from "lucide-react";
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World</h1>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="lets start illustration" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <button
+        type="button"
+        className="py-2.5 px-4 rounded-lg bg-violet-500 text-violet-50 text-sm font-medium flex items-center gap-2 hover:bg-violet-600 transition-colors"
+      >
+        <Plus size={20} />
+        Cadastrar meta
+      </button>
+    </div>
+  );
 }
