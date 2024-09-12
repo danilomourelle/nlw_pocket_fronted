7abc5c652f0ede55aa7373250bb96387c3e0e1ad
Danilo Mourelle <danilomourelle@outlook.com>
2024-09-12 11:10:24 -0300

    feat(app):implements state, effect and fetch to pick page

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import { CreateGoalDialog } from "./components/createGoalDialog";
-// import { EmptyGoals } from "./components/emptyGoals";
+import { EmptyGoals } from "./components/emptyGoals";
 import { Dialog } from "./components/ui/dialog";
 import { WeekSummary } from "./components/weekSummary";
 
+type WeekSummaryType = {
+  completed: number;
+  total: number;
+  goalsPerDay: Record<
+    string,
+    {
+      id: string;
+      title: string;
+      completedAt: string;
+    }[]
+  >;
+};
 export function App() {
+  const [weekSummary, setWeekSummary] = useState<WeekSummaryType | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    fetch("http://localhost:3333/summary")
+      .then((response) => response.json())
+      .then((data) => {
+        setWeekSummary(data);
+      });
+  }, []);
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <WeekSummary />
+      {weekSummary && weekSummary.total > 17 ? <WeekSummary /> : <EmptyGoals />}
       <CreateGoalDialog />
     </Dialog>
   );
