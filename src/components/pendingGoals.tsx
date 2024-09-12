39085cde176f386050db9f08e54c85d8094a142e
Danilo Mourelle <danilomourelle@outlook.com>
2024-09-12 13:24:10 -0300

    feat(summary):handle complete a goal

--- src/components/pendingGoals.tsx
+++ src/components/pendingGoals.tsx
@@ -1,9 +1,12 @@
 import { Plus } from "lucide-react";
 import { OutlineButton } from "./ui/outline-button";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getPendingGoals } from "../http/getPendingGoals";
+import { completeGoal } from "../http/createGoCompletion";
 
 export function PendingGoals() {
+  const queryClient = useQueryClient();
+
   const { data } = useQuery({
     queryKey: ["pending-goals"],
     queryFn: getPendingGoals,
@@ -11,12 +14,19 @@ export function PendingGoals() {
 
   if (!data) return null;
 
+  async function handleCompleteGoal(goalId: string) {
+    await completeGoal(goalId);
+    queryClient.invalidateQueries({ queryKey: ["pending-goals"] });
+    queryClient.invalidateQueries({ queryKey: ["summary"] });
+  }
+
   return (
     <div className="flex flex-wrap gap-3">
       {data.map((goal) => (
         <OutlineButton
           key={goal.id}
           disabled={goal.completionsCount >= goal.desiredWeeklyFrequency}
+          onClick={() => handleCompleteGoal(goal.id)}
         >
           <Plus className="size-4 text-zinc-600" />
           {goal.title}
