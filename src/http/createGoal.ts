0b9f28998426fc844372f19f8e7954134b964a9a
Danilo Mourelle <danilomourelle@outlook.com>
2024-09-13 10:40:33 -0300

    refactor(create-goal):update url and response type

--- src/http/createGoal.ts
+++ src/http/createGoal.ts
@@ -4,14 +4,15 @@ interface CreateGoalRequest {
 }
 
 export async function createGoal(payload: CreateGoalRequest) {
-  const response = await fetch("http://localhost:3333/goals", {
+  const response = await fetch("http://localhost:3333/goal", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ ...payload }),
   });
+
   const data = await response.json();
 
-  return data.goalsProgress;
+  return data;
 }
